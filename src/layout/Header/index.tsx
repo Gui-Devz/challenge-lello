91e22a60a631f3df97ef9534cd5bc34e0785c1e9
gzagari@brasilcash.com.br <gzagari@brasilcash.com.br>
2023-01-30 01:13:08 -0300

    feat: Implementa a UX do header

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,16 +1,82 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { HamburguerMenu } from "../../components/HamburguerMenu";
+import { LinkTag } from "../../components/LinkTag";
 import { Logo } from "../../components/Logo";
-import { HeaderTag } from "./styles";
+import { NavModal } from "../../components/NavModal";
+import { Container, HeaderTag, LinkContainer } from "./styles";
 
 export const Header = () => {
+  const [headerStyles, setHeaderStyles] = useState<string>("default");
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  const listenScrollEvent = (event) => {
+    console.log(window.scrollY);
+
+    if (window.scrollY > 50 && window.scrollY < 420) {
+      setHeaderStyles("variant");
+    } else if (window.scrollY < 50) {
+      setHeaderStyles("default");
+    } else if (window.scrollY >= 420) {
+      setHeaderStyles("showButton");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+
+    return () => window.removeEventListener("scroll", listenScrollEvent);
+  }, []);
   return (
-    <HeaderTag>
-      <ul>
-        <li>
-          <Logo />
-        </li>
-        <li></li>
-        <li></li>
-      </ul>
+    <HeaderTag
+      className="container-fluid text-center p-4 m-0"
+      config={headerStyles}
+    >
+      <Container className="d-flex m-auto position-relative">
+        <div className="d-lg-none position-absolute">
+          <button
+            onClick={handleShow}
+            className="bg-transparent border-0 w-auto"
+          >
+            <HamburguerMenu />
+          </button>
+        </div>
+        <ul className="d-block w-100 d-lg-flex justify-content-between">
+          <li className="first d-flex justify-content-center m-auto m-lg-0">
+            <Logo />
+          </li>
+
+          <div className="d-lg-flex gap-5">
+            <li className="d-none d-lg-block">
+              <Link to="/">Home</Link>
+            </li>
+            <li
+              className={`d-none d-lg-block ${
+                headerStyles !== "showButton" && "me-lg-4"
+              }`}
+            >
+              <Link to="/usuarios">Usuários</Link>
+            </li>
+            <LinkContainer
+              className={`link first d-none ${
+                headerStyles === "showButton" && "d-lg-block"
+              }`}
+            >
+              <LinkTag className="p-2 ps-3 pe-3" type="button" href={"#form"}>
+                Cadastrar
+              </LinkTag>
+            </LinkContainer>
+          </div>
+        </ul>
+      </Container>
+
+      <div className="position-absolute top-0 start-0 w-50 z-index-0">
+        {showModal && <NavModal handleClose={handleClose} />}
+      </div>
     </HeaderTag>
   );
 };
--- src/layout/Header/styles.ts
+++ src/layout/Header/styles.ts
@@ -1,3 +1,79 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const HeaderTag = styled.header``;
+interface IHeaderTagProps {
+  config: string;
+}
+
+export const HeaderTag = styled.header<IHeaderTagProps>`
+  background: rgba(235, 246, 250, 1);
+  width: 100%;
+  position: fixed;
+  top: 0;
+
+  @media (max-width: 1100px) {
+    position: static;
+  }
+  transition: background 250ms ease-in;
+
+  ${(props) =>
+    (props.config === "variant" || props.config === "showButton") &&
+    css`
+      backdrop-filter: blur(15px);
+      -webkit-backdrop-filter: blur(15px);
+      background: rgba(235, 246, 250, 0.3);
+      z-index: 10000;
+    `}
+`;
+
+export const Container = styled.div`
+  max-width: 1170px;
+
+  ul {
+    li {
+      font-weight: 400;
+      font-size: 1.125rem;
+      color: var(--cyan);
+      width: fit-content;
+    }
+
+    a {
+      font-weight: bold;
+    }
+    a:hover {
+      color: var(--cyan);
+    }
+
+    li:not(.first) {
+      position: relative;
+
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: -1px;
+        left: 0;
+        width: 0%;
+        height: 1.5px;
+        transition: all 250ms ease-in;
+
+        background: var(--cyan);
+      }
+
+      &:hover::after {
+        width: 100%;
+      }
+    }
+  }
+`;
+
+export const LinkContainer = styled.li`
+  animation: show 0.25s ease-in-out forwards;
+
+  @keyframes show {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
