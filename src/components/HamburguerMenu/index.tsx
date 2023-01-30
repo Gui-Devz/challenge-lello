5542aac6c6f28c445145fcb925767dbadf800802
gzagari@brasilcash.com.br <gzagari@brasilcash.com.br>
2023-01-30 01:10:30 -0300

    feat: Cria o componente HamburguerMenu

---
+++ src/components/HamburguerMenu/index.tsx
@@ -0,0 +1,12 @@
+export const HamburguerMenu = () => {
+  return (
+    <svg className="pt-1" viewBox="0 0 10 8" width="30">
+      <path
+        d="M1 1h8M1 4h 8M1 7h8"
+        stroke="#707070"
+        strokeWidth="1"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
