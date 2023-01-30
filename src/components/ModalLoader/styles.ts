d56a5e8fe2b389eb7d1bfaceab89676e1f46af5c
gzagari@brasilcash.com.br <gzagari@brasilcash.com.br>
2023-01-30 12:20:02 -0300

    feat: Cria o componente ModalLoader

---
+++ src/components/ModalLoader/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Modal = styled.div`
+  overflow: hidden;
+  position: fixed;
+  left: 0;
+  top: 0;
+
+  right: 0;
+  width: 100vw;
+  min-height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(24px);
+
+  z-index: 1000;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  :hover p {
+    text-decoration: none !important;
+  }
+`;
