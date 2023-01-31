bd3ac89da7e49e74a44938bd899022f0fd458a35
gzagari@brasilcash.com.br <gzagari@brasilcash.com.br>
2023-01-31 11:43:10 -0300

    feat: Adiciona a condicional para o Authorization no header das requests

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 const token = process.env.REACT_APP_GITHUB_ACCESS_TOKEN;
-const headers = {
-  Authorization: `Bearer ${token}`,
-};
+
+let headers = {};
+if (token) {
+  headers = {
+    Authorization: `Bearer ${token}`,
+  };
+}
 
 export const api = axios.create({
   baseURL: "",
